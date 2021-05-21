bba1f4281ae720f7bb3106def659ecbdfdced8c1
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-05-21 17:48:33 -0400

    added myDrodownButton

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(TempertureConverter());
 
-
 class TempertureConverter extends StatefulWidget {
   TempertureConverter({Key key}) : super(key: key);
 
@@ -12,6 +11,7 @@ class TempertureConverter extends StatefulWidget {
 
 class _TempertureConverterState extends State<TempertureConverter> {
   String _temperatureValueFrom = 'Fahrenheit';
+  String _temperatureValueTo = 'Celsius';
   List<String> _itemsTemperature = ['Celsius', 'Fahrenheit'];
   @override
   Widget build(BuildContext context) {
@@ -22,22 +22,41 @@ class _TempertureConverterState extends State<TempertureConverter> {
           title: Text('Material App Bar'),
         ),
         body: Center(
-          child: Column(
-            children: [
-              Text("Value"),
-              TextField(),
-              Text("From"),
-              DropdownButton(value: _temperatureValueFrom,items: _itemsTemperature.map((String value){ return DropdownMenuItem( value: value, child: Text(value)); }).toList(), onChanged: (String value) {
-                setState(() {
-                  _temperatureValueFrom = value;
-                });
-              } ),
-              Text("To"),
-              Text("100.0 ")
-            ]
-          ),
+          child: Column(children: [
+            Text("Value"),
+            TextField(),
+            Text("From"),
+            DropdownButton(
+                value: _temperatureValueFrom,
+                items: _itemsTemperature.map((String value) {
+                  return DropdownMenuItem(value: value, child: Text(value));
+                }).toList(),
+                onChanged: (String value) {
+                  setState(() {
+                    _temperatureValueFrom = value;
+                  });
+                }),
+            Text("To"),
+            myDropdownButton(),
+            Text("100.0 ")
+          ]),
         ),
       ),
+    );
+  }
+
+  Widget myDropdownButton() {
+    return DropdownButton(
+      value: _temperatureValueTo,
+      items: _itemsTemperature.map((e) {
+        return DropdownMenuItem( value: e, child: Text(e));
+      }).toList(),
+      onChanged: (String e) {
+        setState(() {
+           _temperatureValueTo = e;
+        });
+       
+      },
     );
   }
 }
