f4b474d113e4a05f5309388202ff9700fc34f04a
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2021-05-21 18:19:11 -0400

    added onChanged TextField

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class TempertureConverter extends StatefulWidget {
 class _TempertureConverterState extends State<TempertureConverter> {
   String _temperatureValueFrom = 'Fahrenheit';
   String _temperatureValueTo = 'Celsius';
+  String _result = '';
+  double valueInput;
   List<String> _itemsTemperature = ['Celsius', 'Fahrenheit'];
   @override
   Widget build(BuildContext context) {
@@ -24,12 +26,21 @@ class _TempertureConverterState extends State<TempertureConverter> {
         body: Center(
           child: Column(children: [
             Text("Value"),
-            TextField(),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueInput= double.tryParse(value);
+                });
+                
+
+              },
+            ),
             Text("From"),
             DropdownButton(
                 value: _temperatureValueFrom,
                 items: _itemsTemperature.map((String value) {
-                  return DropdownMenuItem(value: value, child: Text(value));
+                  return DropdownMenuItem<String>(
+                      value: value, child: Text(value));
                 }).toList(),
                 onChanged: (String value) {
                   setState(() {
@@ -38,7 +49,13 @@ class _TempertureConverterState extends State<TempertureConverter> {
                 }),
             Text("To"),
             myDropdownButton(),
-            Text("100.0 ")
+            RaisedButton(child: Text('Convert'), onPressed: () {
+              setState(() {
+                  
+                _result = 'Resultado aqui $valueInput';
+              });
+            }),
+            Text(_result)
           ]),
         ),
       ),
@@ -49,13 +66,12 @@ class _TempertureConverterState extends State<TempertureConverter> {
     return DropdownButton(
       value: _temperatureValueTo,
       items: _itemsTemperature.map((e) {
-        return DropdownMenuItem( value: e, child: Text(e));
+        return DropdownMenuItem(value: e, child: Text(e));
       }).toList(),
       onChanged: (String e) {
         setState(() {
-           _temperatureValueTo = e;
+          _temperatureValueTo = e;
         });
-       
       },
     );
   }
